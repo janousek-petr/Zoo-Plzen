7e95f12830b50abd668b85dd88ec09de40e39c93
Minh Tuan Dao <michaldao56@gmail.com>
2026-04-14 11:24:34 +0200

    Přidání modelu Vyplněné otázky
    Přidání modelu Vyplněné otázky

---
+++ backend/app/Models/AnsweredQuestion.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AnsweredQuestion extends Model
+{
+    //
+    protected $table = 'answered_questions';
+    protected $fillable = ['answered_quiz_id', 'question_id', 'chosen_answer', 'written_answer'];
+    protected $timestamp = false;
+
+    public function answered_quiz()
+    {
+        return $this->belongsTo(AnsweredQuizzes::class);
+    }
+    public function question() {
+        return $this->belongsTo(Question::class);
+    }
+}
