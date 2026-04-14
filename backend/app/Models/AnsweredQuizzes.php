95b25621c0f0183782f032615b519e49948ddf1b
Minh Tuan Dao <michaldao56@gmail.com>
2026-04-14 11:35:00 +0200

    Přidání relace ve Vyplněného kvízu
    Přidání funkce answeredQuestions()

--- backend/app/Models/AnsweredQuizzes.php
+++ backend/app/Models/AnsweredQuizzes.php
@@ -10,6 +10,7 @@ class AnsweredQuizzes extends Model
     protected $table = 'answered_quizzes';
 
     protected $fillable = ['quiz_id', 'user_id', 'score'];
+
     protected $dates = ['answered_at'];
 
     protected $timestamp = false;
@@ -22,5 +23,10 @@ class AnsweredQuizzes extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function answeredQuestions()
+    {
+        return $this->hasMany(AnsweredQuestion::class);
     }
 }
