e6d890ac940b79b4f38ffc84c78ff20cd1c6625b
Minh Tuan Dao <michaldao56@gmail.com>
2026-04-17 21:00:19 +0200

    Přidání migrace inventáře
    Přidání migrace inventáře, položky, kategorie položky, oblast a zvíře položky

---
+++ backend/database/migrations/2026_04_17_185621_create_inventory_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('inventory_id')->primary()->constrained('inventory');
+            $table->foreignId('item_id')->constrained('item');
+            $table->date('acquisition_date');
+            $table->date('loss_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_items');
+    }
+};
